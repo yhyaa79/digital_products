93e2edf28e03a1294f416f4a77cec03c79ac0694
yhyaa79 <162740371+yhyaa79@users.noreply.github.com>
2026-02-07 17:22:36 +0330

    update

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
@@ -26,7 +26,7 @@ class Product(models.Model):
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
     
     class Meta():
-        db_table = 'categories'
+        db_table = 'products'
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
 
@@ -40,7 +40,7 @@ class File(models.Model):
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
     
     class Meta():
-        db_table = 'categories'
+        db_table = 'files'
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
 
