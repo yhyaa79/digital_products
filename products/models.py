ed3c1d1029ad041db358b6e38a363bcf7de5a79c
yhyaa79 <162740371+yhyaa79@users.noreply.github.com>
2026-02-07 17:04:22 +0330

    update

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     creted_time = models.DateTimeField(_('creted time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
@@ -17,7 +17,18 @@ class Category(models.Model):
     
 
 class Product(models.Model):
-    pass
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
+    creted_time = models.DateTimeField(_('creted time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    
+    class Meta():
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
 
 
 class File(models.Model):
